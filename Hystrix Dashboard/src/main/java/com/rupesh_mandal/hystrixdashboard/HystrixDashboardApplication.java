f812c8f8838f26f1bd7c6159f1725bf3e793f733
Rupesh Mandal <61101968+Rupesh-Mandal@users.noreply.github.com>
2022-06-09 02:52:39 -0700

    Add files via upload
    completed

--- Hystrix Dashboard/src/main/java/com/rupesh_mandal/hystrixdashboard/HystrixDashboardApplication.java
+++ Hystrix Dashboard/src/main/java/com/rupesh_mandal/hystrixdashboard/HystrixDashboardApplication.java
@@ -1,17 +1,18 @@
-package com.rupesh_mandal.api_gatway;
+package com.rupesh_mandal.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
-public class ApiGatwayApplication {
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatwayApplication.class, args);
+        SpringApplication.run(HystrixDashboardApplication.class, args);
     }
 
 }
